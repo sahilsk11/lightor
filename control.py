fa1c3d83c75233a8128d7a3e1ebc2d4de0190c7b
Sahil <sahilkapur.a@gmail.com>
2016-09-06 18:10:48 -0700

    Updated frontend and relay control

--- control.py
+++ control.py
@@ -2,32 +2,34 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 
-port0 = 3
-port1 = 18
-ports = [port0, port1]
+class relay_control:
+    
+    def __init__(self, ports):
+        self.ports = ports
 
-def set_ports():
-    GPIO.setmode(GPIO.BCM)
-    for port in ports:
-        GPIO.setup(port, GPIO.OUT)
+    def set_ports(self):
+        GPIO.setmode(GPIO.BCM)
+        for port in self.ports:
+            GPIO.setup(port, GPIO.OUT)
+    
+    def start_port(self,port):
+        GPIO.output(port, GPIO.LOW)
+        print "on"
+    
+    def stop_port(port):
+        GPIO.output(port, GPIO.HIGH)
+        print "off"
+    
+    def ports_on():
+        for port in self.ports:
+            start_port(port)
+            time.sleep(1)
 
-def start_port(port):
-    GPIO.output(port, GPIO.LOW)
-    print "on"
-
-def stop_port(port):
-    GPIO.output(port, GPIO.HIGH)
-    print "off"
-
-def ports_on():
-    for port in ports:
-        start_port(port)
-        time.sleep(1)
-
-set_ports()
-ports_on()
-time.sleep(5)
-stop_port(3)
-stop_port(18)
-time.sleep(1)
-print "finished at " + str(datetime.datetime.now())
+if (__main__ == "__name__"):
+    set_ports()
+    ports_on()
+    time.sleep(5)
+    stop_port(3)
+    stop_port(18)
+    time.sleep(1)
+    print "finished at " + str(datetime.datetime.now())
--- conversion.py
+++ conversion.py
@@ -2,8 +2,8 @@ import json
 import urllib
 import passwords
 import datetime
-import calendar
-import time
+import datetime
+from dateutil import tz
 
 def convert_zip(zip_code):
     url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + str(zip_code) + "&key=" + str(passwords.map_api())
@@ -12,19 +12,43 @@ def convert_zip(zip_code):
     load = json.loads(read)
     return load["results"][0]["geometry"]["location"]["lat"], load["results"][0]["geometry"]["location"]["lng"]
 
-def get_sunset_dark(lat, lon):
-    url = "http://api.sunrise-sunset.org/json?formatted=0&lat=%s&lng=%s" % (lat, lon)
+def get_dark_time(lat, lon):
+    now = str(datetime.datetime.now())
+    url = "http://api.sunrise-sunset.org/json?formatted=0&lat=%s&lng=%s&date=%s" % (lat, lon, now)
     fh = urllib.urlopen(url)
     read = fh.read()
     load = json.loads(read)
-    return load["results"]["sunset"], load["results"]["civil_twilight_end"]
+    return load["results"]["civil_twilight_end"]
 
-def utc_2_local(utc):
-        print "utc_2_local: before convert:", utc
-        timestamp =  calendar.timegm((datetime.datetime.strptime( utc, TIME_FORMAT)).timetuple())
-        local = datetime.datetime.fromtimestamp(timestamp).strftime(TIME_FORMAT)
-        print "utc_2_local: after convert:", local
+def get_sunrise(lat, lon):
+    dt_now = datetime.datetime.now()
+    dt_next = dt_now + datetime.timedelta(days=1)
+    dt_next = str(dt_next)
+    url = "http://api.sunrise-sunset.org/json?formatted=0&lat=%s&lng=%s&date=%s" % (lat, lon, dt_next)
+    fh = urllib.urlopen(url)
+    read = fh.read()
+    load = json.loads(read)
+    return load["results"]["astronomical_twilight_begin"]
 
-(lat, lon) = convert_zip(95138)
-(utc_sunset, utc_dark) = get_sunset_dark(lat, lon)
-print utc_2_local(utc_sunset)
+def from_utc(utc):
+    utc = datetime.datetime.strptime(utc, "%Y-%m-%dT%H:%M:%S+00:00")
+    UTC_OFFSET_TIMEDELTA = datetime.datetime.utcnow() - datetime.datetime.now()
+    local = utc - UTC_OFFSET_TIMEDELTA
+    return local
+
+def on_now(lat, lon):
+    current_time = str(datetime.datetime.now())
+    sunset_time = get_dark_time(lat, lon)
+    sunrise_time = get_sunrise(lat, lon)
+    sunset_time = from_utc(sunset_time)
+    sunrise_time = from_utc(sunrise_time)
+    current_time = current_time[:19]
+    current_time = datetime.datetime.strptime(current_time, '%Y-%m-%d %H:%M:%S')
+    return (sunrise_time > current_time > sunset_time)
+
+def format_date(lat, lon):
+    sunrise_time = from_utc(get_sunrise(lat, lon))
+    sunset_time = from_utc(get_dark_time(lat, lon))
+    readable_sunrise_time = datetime.datetime.strftime(sunrise_time, '%I:%M %p')
+    readable_sunset_time = datetime.datetime.strftime(sunset_time, '%I:%M %p')
+    return (readable_sunset_time, readable_sunrise_time)
